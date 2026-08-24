ea71efd488b496926b204d05ed9c38dae55f257c
MrJanHorak <janny_b_goode@hotmail.com>
2026-08-23 21:29:45 -0400

    add middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,11 +1,29 @@
-const registerUser = (req, res) => {
-  res.Send("Register Route")
-}
-const loginUser = (req, res) => {
-  res.Send("Login Route")
-}
+const asyncHandler = require('exrpess-async-handler');
+
+// @desc   Register a new user
+// @route  /api/users
+// @access Public
+const registerUser = asyncHandler(async (req, res) => {
+  console.log(req.body);
+  const { name, email, password } = req.body;
+
+  //Validation
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error('Please include all fields!');
+  }
+
+  res.send('Register Route');
+});
+
+// @desc   Login a user
+// @route  /api/users/login
+// @access Public
+const loginUser = asyncHandler(async (req, res) => {
+  res.send('Login Route');
+});
 
 module.exports = {
   registerUser,
-  loginUser
-}
+  loginUser,
+};
