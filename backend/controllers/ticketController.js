a9a97000f124045f4bec1e4ccae78b10e3b8edf0
MrJanHorak <janny_b_goode@hotmail.com>
2026-08-25 17:35:17 -0400

    add update and delete on tickets on backend aka full crud achieved

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -18,6 +18,87 @@ const getTickets = asyncHandler(async (req, res) => {
   res.status(200).json(tickets);
 });
 
+// @desc   Get user tickets
+// @route  GET /api/tickets/:id
+// @access Private
+const getTicket = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error('User not found');
+  }
+
+  const ticket = await Ticket.findById(req.params.id);
+  if (!ticket) {
+    res.status(404);
+    throw new Error('Ticket not found');
+  }
+
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error('Not authorizes');
+  }
+
+  res.status(200).json(ticket);
+});
+
+// @desc   Delete user tickets
+// @route  DELETE /api/tickets/:id
+// @access Private
+const deleteTicket = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error('User not found');
+  }
+
+  const ticket = await Ticket.findById(req.params.id);
+  if (!ticket) {
+    res.status(404);
+    throw new Error('Ticket not found');
+  }
+
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error('Not authorizes');
+  }
+  await Ticket.findByIdAndDelete(req.params.id);
+  res.status(200).json({ succes: true });
+});
+
+// @desc   Update user tickets
+// @route  PUT /api/tickets/:id
+// @access Private
+const updateTicket = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error('User not found');
+  }
+
+  const ticket = await Ticket.findById(req.params.id);
+  if (!ticket) {
+    res.status(404);
+    throw new Error('Ticket not found');
+  }
+
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error('Not authorizes');
+  }
+
+  const updatedTicket = await Ticket.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true },
+  );
+
+  res.status(200).json(updatedTicket);
+});
+
 // @desc   Create a ticket
 // @route  POST /api/tickets
 // @access Private
@@ -47,5 +128,8 @@ const createTicket = asyncHandler(async (req, res) => {
 
 module.exports = {
   getTickets,
+  getTicket,
   createTicket,
+  deleteTicket,
+  updateTicket,
 };
