335b82d46d401004eef1703ddbae80f5a1e9377a
MrJanHorak <janny_b_goode@hotmail.com>
2026-08-25 12:27:04 -0400

    begin initial redux store setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 
+
 function App() {
   return (
     <>
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react';
 import { FaUser } from 'react-icons/fa';
 import { toast } from 'react-toastify';
 
+import { useSelector, useDispatch } from 'react-redux';
+import { register } from '../features/auth/authSlice';
+
 function Register() {
   const [formData, setFormData] = useState({
     name: '',
@@ -12,6 +15,12 @@ function Register() {
 
   const { name, email, password, password2 } = formData;
 
+  const dispatch = useDispatch();
+
+  const { user, isLoading, isSuccess, message } = useSelector(
+    (state) => state.auth,
+  );
+  
   const onChange = (e) => {
     setFormData((prevState) => ({
       ...prevState,
@@ -24,6 +33,13 @@ function Register() {
 
     if (password !== password2) {
       toast.error('Passwords do not match');
+    } else {
+      const userData = {
+        name,
+        email,
+        password,
+      };
+      dispatch(register(userData));
     }
   };
 
@@ -31,7 +47,7 @@ function Register() {
     <>
       <section className='heading'>
         <h1>
-          <FaUser /> Register
+          <FaUser /> Register {user}
         </h1>
         <p>Please create an account</p>
       </section>
