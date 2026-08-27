9232333d196ea0faa29aa4398c37a6b13f76dd4e
MrJanHorak <janny_b_goode@hotmail.com>
2026-08-26 20:31:18 -0400

    add tickets view page

--- frontend/src/pages/Tickets.jsx
+++ frontend/src/pages/Tickets.jsx
@@ -3,12 +3,13 @@ import { useSelector, useDispatch } from 'react-redux';
 import { getTickets, reset } from '../features/tickets/ticketSlice';
 import Spinner from '../components/Spinner';
 import { BackButton } from '../components/BackButton';
+import TicketItem from '../components/TicketItem';
 
 function Tickets() {
   const { tickets, isLoading, isSuccess } = useSelector(
     (state) => state.tickets,
   );
-
+  console.log(tickets);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -23,11 +24,27 @@ function Tickets() {
     dispatch(getTickets());
   }, [dispatch]);
 
-  if(isLoading){
-    return <Spinner />
+  if (isLoading) {
+    return <Spinner />;
   }
-  
-  return <div>Tickets</div>;
+
+  return (
+    <>
+      <BackButton url='/' />
+      <h1>Tickets</h1>
+      <div className='tickets'>
+        <div className='ticket-headings'>
+          <div>Date</div>
+          <div>Product</div>
+          <div>Status</div>
+          <div></div>
+        </div>
+        {tickets.map((ticket) => (
+          <TicketItem key={ticket._id} ticket={ticket} />
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default Tickets;
