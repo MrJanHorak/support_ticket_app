5dce655360001edb8696faa5d0448cbd1488b302
MrJanHorak <janny_b_goode@hotmail.com>
2026-08-27 22:12:43 -0400

    add notes to Ticket page

--- frontend/src/features/notes/noteService.js
+++ frontend/src/features/notes/noteService.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
-const API_URL = 'api/tickets/';
+const API_URL = '/api/tickets/';
 
 const getNotes = async (ticketId, token) => {
+  console.log(ticketId)
   const config = {
     headers: {
       Authorization: `Bearer ${token}`,
@@ -10,6 +11,7 @@ const getNotes = async (ticketId, token) => {
   };
 
   const response = await axios.get(API_URL + ticketId + '/notes', config);
+  // console.log(response)
   return response.data;
 };
 
--- frontend/src/pages/Ticket.jsx
+++ frontend/src/pages/Ticket.jsx
@@ -1,14 +1,19 @@
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { getTicket, closeTicket } from '../features/tickets/ticketSlice';
+import { getNotes } from '../features/notes/noteSlice';
 import BackButton from '../components/BackButton';
 import Spinner from '../components/Spinner';
+import NoteItem from '../components/Noteitem';
 import { useParams, useNavigate } from 'react-router';
 import { toast } from 'react-toastify';
 
 function Ticket() {
   const { ticket, isLoading, isError, message } = useSelector(
     (state) => state.tickets,
+  );
+  const { notes, isLoading: notesIsLoading } = useSelector(
+    (state) => state.notes,
   );
 
   const navigate = useNavigate();
@@ -20,6 +25,7 @@ function Ticket() {
       toast.error(message);
     }
     dispatch(getTicket(ticketId));
+    dispatch(getNotes(ticketId));
     // eslint-disable-next-line
   }, [isError, message, ticketId]);
 
@@ -29,7 +35,7 @@ function Ticket() {
     navigate('/tickets');
   };
 
-  if (isLoading) {
+  if (isLoading || notesIsLoading) {
     return <Spinner />;
   }
 
@@ -56,7 +62,12 @@ function Ticket() {
           <h3>Description of Issue</h3>
           <p>{ticket.description}</p>
         </div>
+        <h2>Notes</h2>
       </header>
+
+      {notes.map((note) => (
+        <NoteItem key={note._id} note={note} />
+      ))}
 
       {ticket.status !== 'closed' && (
         <button className='btn btn-block btn-danger' onClick={onTicketClose}>
